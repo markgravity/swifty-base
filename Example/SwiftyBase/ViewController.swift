aef9bb3e2b84477bc97d1c68cca13fe9c28055c4
Mark G <markgravity.in@gmail.com>
2020-03-23 17:00:29 +0700

    Initial commit

---
+++ Example/SwiftyBase/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SwiftyBase
+//
+//  Created by markgravity on 03/23/2020.
+//  Copyright (c) 2020 markgravity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
